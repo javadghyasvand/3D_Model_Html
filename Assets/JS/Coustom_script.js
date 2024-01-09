94ae07a15bc5aa3fc2f09ed00f2aa98460308084
javadghyasvand68 <javadghyasvand68@gmail.com>
2024-01-09 12:17:59 +0330

    last update  in   1402.10

--- Assets/JS/Coustom_script.js
+++ Assets/JS/Coustom_script.js
@@ -1,8 +1,14 @@
 $('#category').on('change', function() {
-  this.value;
+    this.value;
 
-     var href=window.location.href.split("_");
-     var callLocatin=href[0]+href[1].replace(href[1],"_"+this.value+".html");
-     window.location.href=callLocatin
+    var href = window.location.href.split("_");
+    var callLocatin = href[0] + href[1].replace(href[1], "_" + this.value + ".html");
+    window.location.href = callLocatin
 });
-  
+
+
+$(document).ready(function() {
+    setTimeout(function() {
+        $(".loader").hide();
+    }, 8000);
+})
